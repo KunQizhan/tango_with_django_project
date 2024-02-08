88c035ea83274886d906eda989295f34a858cf18
“KunQi <2733883z@student.gla.ac.uk>
2024-02-08 17:58:35 +0000

    Fifth Commit

--- rango/views.py
+++ rango/views.py
@@ -27,7 +27,7 @@ def index(request):
     page_list = Page.objects.order_by('-views')[:5]
 
     context_dict = {}
-    context_dict['boldmessage'] = 'Crunchy,creamy,cookie,candy,cupcake!'
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
 
