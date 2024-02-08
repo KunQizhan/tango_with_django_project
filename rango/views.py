ec0be1178d4ed9cd3ba02a9ad94c004f5f3ba734
“KunQi <2733883z@student.gla.ac.uk>
2024-02-08 17:45:21 +0000

    Fifth Commit

--- rango/views.py
+++ rango/views.py
@@ -6,7 +6,6 @@ from rango.forms import CategoryForm
 from rango.models import Category
 from rango.models import Page
 
-from rango.forms import CategoryForm
 from django.shortcuts import redirect
 
 from django.urls import reverse
@@ -17,6 +16,8 @@ from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+
+from datetime import datetime
 
 
 def index(request):
@@ -29,10 +30,17 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy,creamy,cookie,candy,cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    return render(request, 'rango/index.html',context = context_dict)
+
+    response = render(request, 'rango/index.html', context = context_dict)
+    return response
+
+    #return render(request, 'rango/index.html',context = context_dict)
 
 def about(request):
     #return HttpResponse("Rango says here is the about page! <a href='/rango/'>Index</a>")
+    #if request.session.test_cookie_worked():
+        #print("TEST COOKIE WORKED!")
+    request.session.delete_test_cookie()
     print(request.method)
     print(request.user)
     return render(request, 'rango/about.html',{})
@@ -59,7 +67,7 @@ def add_category(request,):
 
         if form.is_valid():
             form.save(commit=True)
-            return redirect('/rango/')
+            return redirect(reverse('rango:index'))
         else:
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})
@@ -102,22 +110,22 @@ def register(request):
             user = user_form.save()
             user.set_password(user.password)
             user.save()
+
             profile = profile_form.save(commit=False)
             profile.user = user
+
             if 'picture' in request.FILES:
                 profile.picture = request.FILES['picture']
             profile.save()
+
             registered = True
         else:
             print(user_form.errors, profile_form.errors)
     else:
         user_form = UserForm()
         profile_form = UserProfileForm()
-    return render(request,
-                  'rango/register.html',
-                  context={'user_form': user_form,
-                           'profile_form':profile_form,
-                           'registered':registered})
+
+    return render(request, 'rango/register.html', context={'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
 
 def user_login(request):
     if request.method == 'POST':
@@ -127,7 +135,7 @@ def user_login(request):
         if user:
             if user.is_active:
                 login(request, user)
-                return redirect('rango:index')
+                return redirect(reverse('rango:index'))
             else:
                 return HttpResponse('Your Rango account is disabled.')
         else:
@@ -137,13 +145,11 @@ def user_login(request):
         return render(request, 'rango/login.html')
 
 def some_view(request):
-    if not request.user.is_authenticated():
+    if request.user.is_authenticated:
         return HttpResponse("You are logged in.")
     else:
         return HttpResponse("You are not logged in.")
-@login_required
-def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+
 
 def user_logout(request):
     logout(request)
@@ -152,3 +158,15 @@ def user_logout(request):
 @login_required
 def restricted(request):
     return render(request, 'rango/restricted.html')
+
+def visitor_handler(request, response):
+    visits = int(request.COOKIES.get('visits', '1'))
+    last_visit_cookie = request.COOKIES.get('last_visit_cookie', str(datetime.now()))
+    last_visit_time = datetime.strftime(last_visit_cookie[:-7],
+                                        "%Y-%m-%d %H:%M:%S")
+    if(datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        response.set_cookie('last_visit',str(datetime.now()))
+    else:
+        response.set_cookie('last_visit', last_visit_cookie)
+    response.set_cookie('visits', visits)
