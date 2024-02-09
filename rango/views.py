c9b711770314ad92b7aaa36c4a9b0571bf5ad507
“KunQi <2733883z@student.gla.ac.uk>
2024-02-09 00:15:58 +0000

    Sixth Commit

--- rango/views.py
+++ rango/views.py
@@ -30,8 +30,9 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-
-    response = render(request, 'rango/index.html', context = context_dict)
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    response = render(request, 'rango/index.html', context=context_dict)
     return response
 
     #return render(request, 'rango/index.html',context = context_dict)
@@ -159,14 +160,20 @@ def user_logout(request):
 def restricted(request):
     return render(request, 'rango/restricted.html')
 
-def visitor_handler(request, response):
-    visits = int(request.COOKIES.get('visits', '1'))
-    last_visit_cookie = request.COOKIES.get('last_visit_cookie', str(datetime.now()))
-    last_visit_time = datetime.strftime(last_visit_cookie[:-7],
-                                        "%Y-%m-%d %H:%M:%S")
-    if(datetime.now() - last_visit_time).days > 0:
+def get_server_side_cookie(request,cookie,default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], "%Y-%m-%d %H:%M:%S")
+
+    if (datetime.now() - last_visit_time).days > 0:
         visits = visits + 1
-        response.set_cookie('last_visit',str(datetime.now()))
+        request.session['last_visit'] = str(datetime.now())
     else:
-        response.set_cookie('last_visit', last_visit_cookie)
-    response.set_cookie('visits', visits)
+        request.session['last_visit'] = last_visit_cookie
+    request.session['visits'] = visits
